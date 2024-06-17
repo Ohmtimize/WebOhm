66e324eb2657f740ad7d2fbf7dbb27d8835f6e05
sara-o2do <sara.fernandez@o2do.be>
2024-06-17 16:03:15 +0200

    added views and homepage

--- ohm/ohm/settings.py
+++ ohm/ohm/settings.py
@@ -53,6 +53,22 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'ohm.urls'
 
+# TEMPLATES = [
+#     {
+#         'BACKEND': 'django.template.backends.django.DjangoTemplates',
+#         'DIRS': [],
+#         'APP_DIRS': True,
+#         'OPTIONS': {
+#             'context_processors': [
+#                 'django.template.context_processors.debug',
+#                 'django.template.context_processors.request',
+#                 'django.contrib.auth.context_processors.auth',
+#                 'django.contrib.messages.context_processors.messages',
+#             ],
+#         },
+#     },
+# ]
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -68,6 +84,7 @@ TEMPLATES = [
         },
     },
 ]
+
 
 WSGI_APPLICATION = 'ohm.wsgi.application'
 
--- ohm/ohmtimize/views.py
+++ ohm/ohmtimize/views.py
@@ -1,6 +1,19 @@
-#from django.shortcuts import render
+from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import *
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the ohmitimize app index.")
+    '''View function for home page of site.'''
+
+    # Generate counts of some of the main objects
+    num_installations = Installation.objects.all().count()
+    num_clients = Client.objects.all().count()
+
+    context = {
+        'num_installations': num_installations,
+        'num_clients': num_clients,
+    }
+
+    #return HttpResponse("Hello, world. You're at the ohmitimize app index.")
+    return render(request, 'index.html', context)
