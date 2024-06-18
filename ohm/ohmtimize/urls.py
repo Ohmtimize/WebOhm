b7a77ea8ce9b854d3f903f381b54dbc30576dc98
sara-o2do <sara.fernandez@o2do.be>
2024-06-18 11:36:59 +0200

    added detail view of clients

--- ohm/ohmtimize/urls.py
+++ ohm/ohmtimize/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('clients/', views.ClientsListView.as_view(), name='clients'),
+    path('clients/<int:pk>/', views.ClientDetailView.as_view(), name='client-detail'),
 ]
+
--- ohm/ohmtimize/views.py
+++ ohm/ohmtimize/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import generic
 
 from .models import *
 
@@ -17,3 +18,12 @@ def index(request):
 
     #return HttpResponse("Hello, world. You're at the ohmitimize app index.")
     return render(request, 'index.html', context)
+
+class ClientsListView(generic.ListView):
+    model = Client
+    # filter a subset of results to show:
+    context_object_name = 'client_list'
+    queryset = Client.objects.filter(memberType='ST')[:5] # Get 5 clients with standard membership
+
+class ClientDetailView(generic.DetailView):
+    model = Client
