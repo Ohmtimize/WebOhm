7a22bef21546f2abe8e4fcb6d32847dc396454e8
Benjamin <Benjamin@DESKTOP-5G29PJS>
2024-11-02 08:26:47 +0100

    connected ohmtimize only to mysql db

--- src/ohmtimize/apps.py
+++ src/ohmtimize/apps.py
@@ -1,10 +1,6 @@
 from django.apps import AppConfig
 
-from ohmtimize.message_broker import mqtt_client
 
 class OhmtimizeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'ohmtimize'
-
-    def ready(self):
-        mqtt_client.start_mqtt()
